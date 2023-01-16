5f14a60324aa65ba257d5a62fcb056fd60f58e0a
20120541 <nhiyenphan99@gmail.com>
2023-01-16 19:16:32 +0700

    config theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
-      Hello World
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
