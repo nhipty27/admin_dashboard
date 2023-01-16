e995afbd59e18d23ad2a98d5a8d40a65738d24e7
20120541 <nhiyenphan99@gmail.com>
2023-01-16 18:39:36 +0700

    start project

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+const clientRoutes = require("./routes/client")
+const generalRoutes = require("./routes/general")
+const managementRoutes = require("./routes/management")
+const salesRoutes = require("./routes/sales")
+
+// data imports
+
+/* CONFIGURATION */
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+/* ROUTES */
+app.use("/client", clientRoutes)
+app.use("/general", generalRoutes)
+app.use("/management", managementRoutes)
+app.use("/sales", salesRoutes)
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+
+    /* ONLY ADD DATA ONE TIME */
+    // AffiliateStat.insertMany(dataAffiliateStat)
+    // OverallStat.insertMany(dataOverallStat)
+    // Product.insertMany(dataProduct)
+    // ProductStat.insertMany(dataProductStat)
+    // Transaction.insertMany(dataTransaction)
+    // User.insertMany(dataUser)
+  })
+  .catch((error) => console.log(`${error} did not connect`))
