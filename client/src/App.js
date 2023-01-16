3938334f01e1a44007df8f1edeaf3d0ada3c48ed
20120541 <nhiyenphan99@gmail.com>
2023-01-16 20:10:37 +0700

    sidebar navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import Layout from './pages/layout'
+import Dashboard from './pages/dashboard'
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -13,7 +15,12 @@ function App() {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
         </ThemeProvider>
       </BrowserRouter>
     </div>
